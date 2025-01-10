27739c036549b8ca0eedd6898f543e9d8a6fb539
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2025-01-10 14:39:22 +0100

    fixed the navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="navbar">
+    <div>
+      <img src="../assets/logo-bookmark.svg" alt="" />
+    </div>
+    <div class="links">
+      <p class="link">Features</p>
+      <p class="link">Pricing</p>
+      <p class="link">Contact</p>
+      <myButton text="LOGIN" color="red" />
+    </div>
+  </div>
+</template>
+
+<style>
+.navbar {
+  display: flex;
+  align-items: center;
+  padding: 20px 0;
+  justify-content: space-between;
+}
+
+.links {
+  display: flex;
+  align-items: center;
+  gap: 50px;
+}
+
+.link {
+  cursor: pointer;
+  text-transform: uppercase;
+  letter-spacing: 1.125px;
+  font-weight:500;
+}
+.link:hover {
+  color: var(--primary-soft-red);
+}
+</style>
+
+<script setup>
+import myButton from "./myButton.vue";
+</script>
---
+++ src/components/myButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button :class="color">{{ text }}</button>
+</template>
+
+<style scoped>
+button {
+  padding: 6px 35px;
+  cursor: pointer;
+  border-radius: 5px;
+  background-color: transparent;
+  font-weight:500;
+}
+.red:hover {
+  color: var(--primary-soft-red);
+  border-color: var(--primary-soft-red);
+}
+</style>
+
+<script setup>
+defineProps({
+  text: String,
+  color: String,
+});
+</script>
